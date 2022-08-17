88392abc75454ea0c6144d85f88128d46d2978ab
ahmedfawzy286 <ahmedfawzy286@gmail.com>
2022-08-17 23:15:46 +0200

    commit from master master

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image... and we will try to upload it to nexus repository"
     withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 207.154.251.118:8083/new-test:700 .'
+        sh 'docker build -t 207.154.251.118:8083/floki:master .'
         sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
-        sh 'docker push 207.154.251.118:8083/new-test:700'
+        sh 'docker push 207.154.251.118:8083/floki:master'
     }
 } 
 
