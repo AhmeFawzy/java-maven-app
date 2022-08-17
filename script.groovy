284a3c4fb354077b52f0833750ce0f78b5ea8c51
Ahmed Fawzy <ahmedfawzy286@gmail.com>
2022-08-17 21:48:02 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 207.154.251.118:8083/floki:jenkins-jobs .'
+        sh 'docker build -t 207.154.251.118:8083/microsoft:jenkins-jobs .'
         sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
         sh 'docker push 207.154.251.118:8083/microsoft:jenkins-jobs'
     }
