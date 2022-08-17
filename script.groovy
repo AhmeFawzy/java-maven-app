2ea0ff29c75cef932985f344901abf2b4edc926c
Ahmed Fawzy <ahmedfawzy286@gmail.com>
2022-08-17 12:43:02 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,20 @@
 def buildJar() {
     echo "building the application..."
+    sh 'mvn test'
     sh 'mvn package'
 } 
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "building the docker image... and we will try to upload it to nexus repository"
     withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 207.154.251.118:8083/from-jenkins:700 .'
+        sh 'docker build -t 207.154.251.118:8083/new-test:700 .'
         sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
-        sh 'docker push 207.154.251.118:8083/from-jenkins:700'
+        sh 'docker push 207.154.251.118:8083/new-test:700'
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo 'deploying the application...and the image successfully uploaded to nexus repository, go and check it now '
 } 
 
 return this
