050bb250d3c9fe1246680f0ff70f1e536ab1c59c
Ahmed Fawzy <ahmedfawzy286@gmail.com>
2022-08-17 12:21:51 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t my-repo:jma-3.60 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
         sh 'docker push my-repo:jma-3.60'
     }
 } 
