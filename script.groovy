9da659dfbdcfdba6cda5dac79d7c8006bfbaf608
Ahmed Fawzy <ahmedfawzy286@gmail.com>
2022-08-17 12:21:38 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t flokiboats/my-repo:jma-4.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
         sh 'docker push flokiboats/my-repo:jma-4.0'
     }
 } 
