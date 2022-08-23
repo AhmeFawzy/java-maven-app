c92ae16be4e4ecf7b83f41b295a869774db58aa5
Ahmed Fawzy <ahmedfawzy286@gmail.com>
2022-08-23 01:51:53 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,20 +1,20 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn test'
-    sh 'mvn package'
-} 
+//def buildJar() {
+ //   echo "building the application..."
+  //  sh 'mvn test'
+  //  sh 'mvn package'
+//} 
 
-def buildImage() {
-    echo "building the docker image... and we will try to upload it to nexus repository"
-    withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 207.154.251.118:8083/microsoft:master .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
-        sh 'docker push 207.154.251.118:8083/microsoft:master'
-    }
-} 
+//def buildImage() {
+  //  echo "building the docker image... and we will try to upload it to nexus repository"
+    //withCredentials([usernamePassword(credentialsId: 'nexus-admin-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //    sh 'docker build -t 207.154.251.118:8083/microsoft:master .'
+    //    sh "echo $PASS | docker login -u $USER --password-stdin 207.154.251.118:8083"
+     //   sh 'docker push 207.154.251.118:8083/microsoft:master'
+  //  }
+//} 
 
-def deployApp() {
-    echo 'deploying the application...and the image successfully uploaded to nexus repository, go and check it now '
-} 
+//def deployApp() {
+   // echo 'deploying the application...and the image successfully uploaded to nexus repository, go and check it now '
+//} 
 
-return this
+//return this
