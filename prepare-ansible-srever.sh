b442639704ccb9ffa4c908ac4a9dce12ee950fc0
ahmedfawzy286 <ahmedfawzy286@gmail.com>
2023-02-10 03:58:38 +0200

    shell script for installing pip&boto&botocore

--- prepare-ansible-srever.sh
+++ prepare-ansible-srever.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt update
-apt install ansible -y
+apt install ansible -y   #-y to auto confirm (the prompt to type yes to continue installing it)
 apt install python3-pip -y
 pip3 install boto3 botocore
